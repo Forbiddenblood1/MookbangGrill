ffd0c338da09b429a1a850057b38d1bb2d1d5624
Adriane <buenafloradriane@gmail.com>
2025-11-21 19:57:10 +0800

    Add files via upload

---
+++ UC_Reporting_Analytics.Designer.vb
@@ -0,0 +1,267 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UC_Reporting_Analytics
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        TableLayoutPanel1 = New TableLayoutPanel()
+        Panel1 = New Panel()
+        Panel2 = New Panel()
+        Panel3 = New Panel()
+        Panel4 = New Panel()
+        FilterComboBox = New ComboBox()
+        SumPanelTotalSales = New Panel()
+        lblTotalSalesValue = New Label()
+        lblTotalSalesTitle = New Label()
+        SumOrderCountPanel = New Panel()
+        lblOrderCountValue = New Label()
+        lblOrderCountTitle = New Label()
+        SumAvgCheckSizePanel = New Panel()
+        lblAvgCheckSizeValue = New Label()
+        lblAvgCheckSizeTitle = New Label()
+        AnalDashboardLabel = New Label()
+        FilterLabeel = New Label()
+        TableLayoutPanel1.SuspendLayout()
+        SumPanelTotalSales.SuspendLayout()
+        SumOrderCountPanel.SuspendLayout()
+        SumAvgCheckSizePanel.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' TableLayoutPanel1
+        ' 
+        TableLayoutPanel1.BackColor = Color.MistyRose
+        TableLayoutPanel1.ColumnCount = 2
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 50F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 50F))
+        TableLayoutPanel1.Controls.Add(Panel1, 0, 0)
+        TableLayoutPanel1.Controls.Add(Panel2, 1, 0)
+        TableLayoutPanel1.Controls.Add(Panel3, 0, 1)
+        TableLayoutPanel1.Controls.Add(Panel4, 1, 1)
+        TableLayoutPanel1.Location = New Point(213, 146)
+        TableLayoutPanel1.Name = "TableLayoutPanel1"
+        TableLayoutPanel1.RowCount = 2
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 50F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 50F))
+        TableLayoutPanel1.Size = New Size(811, 564)
+        TableLayoutPanel1.TabIndex = 0
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Dock = DockStyle.Fill
+        Panel1.Location = New Point(3, 3)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(399, 276)
+        Panel1.TabIndex = 0
+        ' 
+        ' Panel2
+        ' 
+        Panel2.Dock = DockStyle.Fill
+        Panel2.Location = New Point(408, 3)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(400, 276)
+        Panel2.TabIndex = 1
+        ' 
+        ' Panel3
+        ' 
+        Panel3.Dock = DockStyle.Fill
+        Panel3.Location = New Point(3, 285)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(399, 276)
+        Panel3.TabIndex = 2
+        ' 
+        ' Panel4
+        ' 
+        Panel4.Dock = DockStyle.Fill
+        Panel4.Location = New Point(408, 285)
+        Panel4.Name = "Panel4"
+        Panel4.Size = New Size(400, 276)
+        Panel4.TabIndex = 3
+        ' 
+        ' FilterComboBox
+        ' 
+        FilterComboBox.DropDownStyle = ComboBoxStyle.DropDownList
+        FilterComboBox.FlatStyle = FlatStyle.Flat
+        FilterComboBox.Font = New Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        FilterComboBox.FormattingEnabled = True
+        FilterComboBox.IntegralHeight = False
+        FilterComboBox.ItemHeight = 20
+        FilterComboBox.Items.AddRange(New Object() {"Current", "Last 7 Days", "This Month", "This Year"})
+        FilterComboBox.Location = New Point(747, 64)
+        FilterComboBox.Name = "FilterComboBox"
+        FilterComboBox.Size = New Size(257, 28)
+        FilterComboBox.TabIndex = 3
+        ' 
+        ' SumPanelTotalSales
+        ' 
+        SumPanelTotalSales.BackColor = Color.DodgerBlue
+        SumPanelTotalSales.BorderStyle = BorderStyle.FixedSingle
+        SumPanelTotalSales.Controls.Add(lblTotalSalesValue)
+        SumPanelTotalSales.Controls.Add(lblTotalSalesTitle)
+        SumPanelTotalSales.Location = New Point(0, 543)
+        SumPanelTotalSales.Name = "SumPanelTotalSales"
+        SumPanelTotalSales.Size = New Size(207, 167)
+        SumPanelTotalSales.TabIndex = 4
+        ' 
+        ' lblTotalSalesValue
+        ' 
+        lblTotalSalesValue.AutoSize = True
+        lblTotalSalesValue.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblTotalSalesValue.Location = New Point(57, 52)
+        lblTotalSalesValue.Name = "lblTotalSalesValue"
+        lblTotalSalesValue.Size = New Size(72, 37)
+        lblTotalSalesValue.TabIndex = 1
+        lblTotalSalesValue.Text = "0.00"
+        ' 
+        ' lblTotalSalesTitle
+        ' 
+        lblTotalSalesTitle.AutoSize = True
+        lblTotalSalesTitle.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblTotalSalesTitle.Location = New Point(41, 6)
+        lblTotalSalesTitle.Name = "lblTotalSalesTitle"
+        lblTotalSalesTitle.Size = New Size(100, 25)
+        lblTotalSalesTitle.TabIndex = 0
+        lblTotalSalesTitle.Text = "Total Sales"
+        ' 
+        ' SumOrderCountPanel
+        ' 
+        SumOrderCountPanel.BackColor = Color.SeaGreen
+        SumOrderCountPanel.BorderStyle = BorderStyle.FixedSingle
+        SumOrderCountPanel.Controls.Add(lblOrderCountValue)
+        SumOrderCountPanel.Controls.Add(lblOrderCountTitle)
+        SumOrderCountPanel.Location = New Point(0, 344)
+        SumOrderCountPanel.Name = "SumOrderCountPanel"
+        SumOrderCountPanel.Size = New Size(207, 167)
+        SumOrderCountPanel.TabIndex = 5
+        ' 
+        ' lblOrderCountValue
+        ' 
+        lblOrderCountValue.AutoSize = True
+        lblOrderCountValue.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblOrderCountValue.Location = New Point(79, 52)
+        lblOrderCountValue.Name = "lblOrderCountValue"
+        lblOrderCountValue.Size = New Size(33, 37)
+        lblOrderCountValue.TabIndex = 1
+        lblOrderCountValue.Text = "0"
+        ' 
+        ' lblOrderCountTitle
+        ' 
+        lblOrderCountTitle.AutoSize = True
+        lblOrderCountTitle.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblOrderCountTitle.Location = New Point(42, 6)
+        lblOrderCountTitle.Name = "lblOrderCountTitle"
+        lblOrderCountTitle.Size = New Size(117, 25)
+        lblOrderCountTitle.TabIndex = 0
+        lblOrderCountTitle.Text = "Order Count"
+        ' 
+        ' SumAvgCheckSizePanel
+        ' 
+        SumAvgCheckSizePanel.BackColor = Color.Orange
+        SumAvgCheckSizePanel.BorderStyle = BorderStyle.FixedSingle
+        SumAvgCheckSizePanel.Controls.Add(lblAvgCheckSizeValue)
+        SumAvgCheckSizePanel.Controls.Add(lblAvgCheckSizeTitle)
+        SumAvgCheckSizePanel.Location = New Point(0, 146)
+        SumAvgCheckSizePanel.Name = "SumAvgCheckSizePanel"
+        SumAvgCheckSizePanel.Size = New Size(207, 167)
+        SumAvgCheckSizePanel.TabIndex = 6
+        ' 
+        ' lblAvgCheckSizeValue
+        ' 
+        lblAvgCheckSizeValue.AutoSize = True
+        lblAvgCheckSizeValue.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblAvgCheckSizeValue.Location = New Point(66, 52)
+        lblAvgCheckSizeValue.Name = "lblAvgCheckSizeValue"
+        lblAvgCheckSizeValue.Size = New Size(72, 37)
+        lblAvgCheckSizeValue.TabIndex = 1
+        lblAvgCheckSizeValue.Text = "0.00"
+        ' 
+        ' lblAvgCheckSizeTitle
+        ' 
+        lblAvgCheckSizeTitle.AutoSize = True
+        lblAvgCheckSizeTitle.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lblAvgCheckSizeTitle.Location = New Point(30, 6)
+        lblAvgCheckSizeTitle.Name = "lblAvgCheckSizeTitle"
+        lblAvgCheckSizeTitle.Size = New Size(139, 25)
+        lblAvgCheckSizeTitle.TabIndex = 0
+        lblAvgCheckSizeTitle.Text = "Avg Check Size"
+        ' 
+        ' AnalDashboardLabel
+        ' 
+        AnalDashboardLabel.AutoSize = True
+        AnalDashboardLabel.Font = New Font("Segoe UI", 27.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        AnalDashboardLabel.Location = New Point(3, 15)
+        AnalDashboardLabel.Name = "AnalDashboardLabel"
+        AnalDashboardLabel.Size = New Size(380, 50)
+        AnalDashboardLabel.TabIndex = 7
+        AnalDashboardLabel.Text = "Analytics DashBoard"
+        ' 
+        ' FilterLabeel
+        ' 
+        FilterLabeel.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        FilterLabeel.Location = New Point(621, 63)
+        FilterLabeel.Name = "FilterLabeel"
+        FilterLabeel.Size = New Size(120, 28)
+        FilterLabeel.TabIndex = 8
+        FilterLabeel.Text = "Select Filter:"
+        ' 
+        ' UC_Reporting_Analytics
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Control
+        Controls.Add(FilterLabeel)
+        Controls.Add(AnalDashboardLabel)
+        Controls.Add(SumAvgCheckSizePanel)
+        Controls.Add(SumOrderCountPanel)
+        Controls.Add(SumPanelTotalSales)
+        Controls.Add(FilterComboBox)
+        Controls.Add(TableLayoutPanel1)
+        Name = "UC_Reporting_Analytics"
+        Size = New Size(1024, 768)
+        TableLayoutPanel1.ResumeLayout(False)
+        SumPanelTotalSales.ResumeLayout(False)
+        SumPanelTotalSales.PerformLayout()
+        SumOrderCountPanel.ResumeLayout(False)
+        SumOrderCountPanel.PerformLayout()
+        SumAvgCheckSizePanel.ResumeLayout(False)
+        SumAvgCheckSizePanel.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents FilterComboBox As ComboBox
+    Friend WithEvents SumPanelTotalSales As Panel
+    Friend WithEvents SumOrderCountPanel As Panel
+    Friend WithEvents SumAvgCheckSizePanel As Panel
+    Friend WithEvents lblTotalSalesValue As Label
+    Friend WithEvents lblTotalSalesTitle As Label
+    Friend WithEvents lblOrderCountValue As Label
+    Friend WithEvents lblOrderCountTitle As Label
+    Friend WithEvents lblAvgCheckSizeValue As Label
+    Friend WithEvents lblAvgCheckSizeTitle As Label
+    Friend WithEvents AnalDashboardLabel As Label
+    Friend WithEvents FilterLabeel As Label
+
+End Class
---
+++ UC_Reporting_Analytics.vb
@@ -0,0 +1,376 @@
+﻿Imports System.Windows.Forms.DataVisualization.Charting
+Imports System.Data.SqlClient
+Imports System.Globalization
+Imports System.Drawing
+
+Public Class UC_Reporting_Analytics
+
+    Private connectionString As String = "Server=localhost\SQLEXPRESS;Database=POS;Integrated Security=True;TrustServerCertificate=True;"
+
+    Private TotalSalesChart As Chart
+    Private OrderCountChart As Chart
+    Private AvgCheckSizeChart As Chart
+    Private TopSellingItemChart As Chart
+
+    Private Const AvgCheckSizeTarget As Decimal = 35D
+
+    Private WithEvents autoRefreshTimer As New Timer()
+
+    'DATA CLASSES
+    Public Class SalesDataPoint
+        Public Property DateValue As DateTime
+        Public Property SalesAmount As Decimal
+        Public Property OrderCount As Integer
+        Public Property AvgCheckSize As Decimal
+        Public Property TargetValue As Decimal
+
+        Public Sub New(dateValue As DateTime, salesAmount As Decimal, orderCount As Integer, avgCheckSize As Decimal, targetValue As Decimal)
+            Me.DateValue = dateValue
+            Me.SalesAmount = salesAmount
+            Me.OrderCount = orderCount
+            Me.AvgCheckSize = avgCheckSize
+            Me.TargetValue = targetValue
+        End Sub
+    End Class
+
+    Public Class TopItemSale
+        Public Property ItemName As String
+        Public Property ItemsSold As Integer
+
+        Public Sub New(itemName As String, itemsSold As Integer)
+            Me.ItemName = itemName
+            Me.ItemsSold = itemsSold
+        End Sub
+    End Class
+
+
+    Public Sub New()
+        InitializeComponent()
+        InitializeChartControls()
+        InitializeFilterComboBox()
+
+        autoRefreshTimer.Interval = 30000
+        autoRefreshTimer.Start()
+        ApplyFilter()
+    End Sub
+
+    ' FILTER COMBOBOX
+    Private Sub InitializeFilterComboBox()
+        FilterComboBox.Items.Clear()
+        FilterComboBox.Items.AddRange({"Current Day", "Last 7 Days", "This Month", "This Year"})
+        FilterComboBox.SelectedIndex = 0
+        AddHandler FilterComboBox.SelectedIndexChanged, AddressOf FilterComboBox_SelectedIndexChanged
+    End Sub
+
+    Private Sub FilterComboBox_SelectedIndexChanged(sender As Object, e As EventArgs)
+        ApplyFilter()
+    End Sub
+
+    Private Sub ApplyFilter()
+        Dim startDate As DateTime
+        Dim endDate As DateTime = DateTime.Now.Date
+        Dim titlePrefix As String = ""
+
+        Select Case FilterComboBox.SelectedItem?.ToString()
+            Case "Current Day"
+                startDate = DateTime.Today
+                endDate = DateTime.Today.AddDays(1)
+
+            Case "Last 7 Days"
+                startDate = endDate.AddDays(-6)
+                titlePrefix = "Last 7 Days"
+
+            Case "This Month"
+                startDate = New Date(endDate.Year, endDate.Month, 1)
+                titlePrefix = "This Month"
+
+            Case "This Year"
+                startDate = New Date(endDate.Year, 1, 1)
+                titlePrefix = "This Year"
+
+            Case Else
+                startDate = endDate.AddDays(-6)
+                titlePrefix = "Last 7 Days"
+        End Select
+
+        LoadAnalyticsData(startDate, endDate, titlePrefix)
+    End Sub
+
+    '   LOADING ANALYTICS
+    Private Sub LoadAnalyticsData(startDate As DateTime, endDate As DateTime, titlePrefix As String)
+
+        Dim timeSeriesData = GetTimeSeriesData(startDate, endDate)
+        Dim totalEarnings As Decimal = GetTotalEarnings(startDate, endDate)
+        Dim itemData = GetTopItemData(startDate, endDate)
+
+        Dim totalOrders As Integer = timeSeriesData.Sum(Function(x) x.OrderCount)
+        Dim avgCheckSize As Decimal = If(totalOrders > 0, totalEarnings / totalOrders, 0D)
+
+        lblTotalSalesValue.Text = totalEarnings.ToString("C2")
+        lblOrderCountValue.Text = totalOrders.ToString()
+        lblAvgCheckSizeValue.Text = avgCheckSize.ToString("C2")
+
+        ' Total Sales
+        TotalSalesChart.DataSource = timeSeriesData
+        TotalSalesChart.Series(0).XValueMember = "DateValue"
+        TotalSalesChart.Series(0).YValueMembers = "SalesAmount"
+        TotalSalesChart.DataBind()
+        TotalSalesChart.Titles(0).Text = $"Total Sales - {titlePrefix}"
+
+        ' Order Count 
+        OrderCountChart.DataSource = timeSeriesData
+        OrderCountChart.Series(0).XValueMember = "DateValue"
+        OrderCountChart.Series(0).YValueMembers = "OrderCount"
+        OrderCountChart.DataBind()
+        OrderCountChart.Titles(0).Text = $"Order Count - {titlePrefix}"
+
+        ' Avg Check Size
+        AvgCheckSizeChart.Titles(0).Text = "Average Check Size"
+        ApplyAvgCheckSizeConditionalColors(timeSeriesData)
+
+        ' Top Selling Items
+        TopSellingItemChart.DataSource = itemData
+        TopSellingItemChart.Series(0).XValueMember = "ItemName"
+        TopSellingItemChart.Series(0).YValueMembers = "ItemsSold"
+        TopSellingItemChart.DataBind()
+        TopSellingItemChart.Titles(0).Text = $"Top Selling Items - {titlePrefix}"
+
+    End Sub
+
+    ' SQL HELPERS
+    Private Function DayStart(d As DateTime) As DateTime
+        Return d.Date
+    End Function
+
+    Private Function DayEnd(d As DateTime) As DateTime
+        Return d.Date.AddDays(1)
+    End Function
+
+    Private Function GetTotalEarnings(startDate As DateTime, endDate As DateTime) As Decimal
+        Using conn As New SqlConnection(connectionString)
+            conn.Open()
+
+            Dim query As String = "
+                SELECT SUM(OI.Subtotal)
+                FROM Orders O
+                INNER JOIN OrderItems OI ON O.OrderID = OI.OrderID
+                WHERE O.CreatedAt >= @Start AND O.CreatedAt < @End
+                AND (O.OrderStatus='Completed' OR O.OrderStatus='Paid')
+            "
+
+            Using cmd As New SqlCommand(query, conn)
+                cmd.Parameters.AddWithValue("@Start", DayStart(startDate))
+                cmd.Parameters.AddWithValue("@End", DayEnd(endDate))
+
+                Dim result = cmd.ExecuteScalar()
+                If result IsNot DBNull.Value AndAlso result IsNot Nothing Then
+                    Return Convert.ToDecimal(result)
+                End If
+            End Using
+        End Using
+
+        Return 0D
+    End Function
+
+
+    Private Function GetTimeSeriesData(startDate As DateTime, endDate As DateTime) As List(Of SalesDataPoint)
+        Dim result As New List(Of SalesDataPoint)
+
+        Using conn As New SqlConnection(connectionString)
+            conn.Open()
+
+            Dim query As String = "
+                SELECT CAST(O.CreatedAt AS DATE) AS SalesDate,
+                       SUM(OI.Subtotal) AS TotalSales,
+                       COUNT(DISTINCT O.OrderID) AS OrderCount
+                FROM Orders O
+                INNER JOIN OrderItems OI ON O.OrderID = OI.OrderID
+                WHERE O.CreatedAt >= @Start AND O.CreatedAt < @End
+                AND (O.OrderStatus='Completed' OR O.OrderStatus='Paid')
+                GROUP BY CAST(O.CreatedAt AS DATE)
+                ORDER BY SalesDate
+            "
+
+            Using cmd As New SqlCommand(query, conn)
+                cmd.Parameters.AddWithValue("@Start", DayStart(startDate))
+                cmd.Parameters.AddWithValue("@End", DayEnd(endDate))
+
+                Using r = cmd.ExecuteReader()
+                    While r.Read()
+                        Dim sales = If(IsDBNull(r("TotalSales")), 0D, Convert.ToDecimal(r("TotalSales")))
+                        Dim orders = If(IsDBNull(r("OrderCount")), 0, Convert.ToInt32(r("OrderCount")))
+                        Dim avgSize As Decimal = If(orders > 0, Decimal.Round(sales / orders, 2), 0D)
+
+                        result.Add(New SalesDataPoint(
+                            Convert.ToDateTime(r("SalesDate")),
+                            sales,
+                            orders,
+                            avgSize,
+                            AvgCheckSizeTarget
+                        ))
+                    End While
+                End Using
+            End Using
+        End Using
+
+
+        Dim filled As New List(Of SalesDataPoint)
+        Dim currentDate = startDate
+
+        While currentDate <= endDate
+            Dim found = result.FirstOrDefault(Function(x) x.DateValue.Date = currentDate.Date)
+
+            If found IsNot Nothing Then
+                filled.Add(found)
+            Else
+                filled.Add(New SalesDataPoint(currentDate, 0, 0, 0, AvgCheckSizeTarget))
+            End If
+
+            currentDate = currentDate.AddDays(1)
+        End While
+
+        Return filled
+    End Function
+
+
+    Private Function GetTopItemData(startDate As DateTime, endDate As DateTime) As List(Of TopItemSale)
+        Dim result As New List(Of TopItemSale)
+
+        Using conn As New SqlConnection(connectionString)
+            conn.Open()
+
+            Dim query As String = "
+                SELECT P.ProductName, SUM(OI.Quantity) AS ItemsSold
+                FROM OrderItems OI
+                INNER JOIN Orders O ON OI.OrderID = O.OrderID
+                INNER JOIN Products P ON OI.ProductID = P.ProductID
+                WHERE O.CreatedAt >= @Start AND O.CreatedAt < @End
+                AND (O.OrderStatus='Completed' OR O.OrderStatus='Paid')
+                GROUP BY P.ProductName
+                ORDER BY ItemsSold DESC
+            "
+
+            Using cmd As New SqlCommand(query, conn)
+                cmd.Parameters.AddWithValue("@Start", DayStart(startDate))
+                cmd.Parameters.AddWithValue("@End", DayEnd(endDate))
+
+                Using r = cmd.ExecuteReader()
+                    While r.Read()
+                        result.Add(New TopItemSale(
+                            r("ProductName").ToString(),
+                            Convert.ToInt32(r("ItemsSold"))
+                        ))
+                    End While
+                End Using
+            End Using
+        End Using
+
+        Return result
+    End Function
+
+    ' AVG CHECK SIZE COLORS
+    Private Sub ApplyAvgCheckSizeConditionalColors(data As List(Of SalesDataPoint))
+        Dim actualSeries = AvgCheckSizeChart.Series("Average Check Size")
+        Dim targetSeries = AvgCheckSizeChart.Series("Target: ₱" & AvgCheckSizeTarget.ToString("N2"))
+
+        actualSeries.Points.Clear()
+        targetSeries.Points.Clear()
+
+        For Each dp In data
+            Dim p As New DataPoint(dp.DateValue.ToOADate(), dp.AvgCheckSize)
+
+            If dp.AvgCheckSize >= AvgCheckSizeTarget Then
+                p.Color = Color.Green
+            Else
+                p.Color = Color.OrangeRed
+            End If
+
+            actualSeries.Points.Add(p)
+            targetSeries.Points.AddXY(dp.DateValue.ToOADate(), dp.TargetValue)
+        Next
+    End Sub
+
+    ' CHART INITIALIZATION
+    Private Sub InitializeChartControls()
+
+
+        TotalSalesChart = New Chart() With {.Dock = DockStyle.Fill}
+        Dim totalArea As New ChartArea("TotalSalesArea")
+        TotalSalesChart.ChartAreas.Add(totalArea)
+
+        TotalSalesChart.Series.Add(New Series("Total Sales") With {
+            .ChartType = SeriesChartType.Line,
+            .XValueType = ChartValueType.Date,
+            .ToolTip = "Date: #VALX{dd MMM yyyy}" & vbCrLf &
+                       "Sales: ₱#VALY{N2}"
+        })
+
+        TotalSalesChart.Titles.Add("Total Sales")
+        Panel1.Controls.Add(TotalSalesChart)
+
+
+
+        OrderCountChart = New Chart() With {.Dock = DockStyle.Fill}
+        Dim orderArea As New ChartArea("OrderCountArea")
+        OrderCountChart.ChartAreas.Add(orderArea)
+
+        OrderCountChart.Series.Add(New Series("Order Count") With {
+            .ChartType = SeriesChartType.Line,
+            .XValueType = ChartValueType.Date,
+            .ToolTip = "Date: #VALX{dd MMM yyyy}" & vbCrLf &
+                       "Orders: #VALY"
+        })
+
+        OrderCountChart.Titles.Add("Order Count")
+        Panel2.Controls.Add(OrderCountChart)
+
+
+
+        AvgCheckSizeChart = New Chart() With {.Dock = DockStyle.Fill}
+        Dim avgArea As New ChartArea("AvgCheckArea")
+        AvgCheckSizeChart.ChartAreas.Add(avgArea)
+
+        AvgCheckSizeChart.Series.Add(New Series("Average Check Size") With {
+            .ChartType = SeriesChartType.Line,
+            .XValueType = ChartValueType.Date,
+            .ToolTip = "Date: #VALX{dd MMM yyyy}" & vbCrLf &
+                       "Avg Check: ₱#VALY{N2}"
+        })
+
+        AvgCheckSizeChart.Series.Add(New Series("Target: ₱" & AvgCheckSizeTarget.ToString("N2")) With {
+            .ChartType = SeriesChartType.Line,
+            .BorderDashStyle = ChartDashStyle.Dash,
+            .XValueType = ChartValueType.Date,
+            .ToolTip = "Target: ₱#VALY{N2}"
+        })
+
+        AvgCheckSizeChart.Titles.Add("Average Check Size")
+        Panel3.Controls.Add(AvgCheckSizeChart)
+
+
+
+        TopSellingItemChart = New Chart() With {.Dock = DockStyle.Fill}
+        Dim topArea As New ChartArea("TopItemArea")
+        TopSellingItemChart.ChartAreas.Add(topArea)
+
+        TopSellingItemChart.Series.Add(New Series("Top Items") With {
+            .ChartType = SeriesChartType.Column,
+            .ToolTip = "Item: #VALX" & vbCrLf &
+                       "Sold: #VALY"
+        })
+
+        TopSellingItemChart.Titles.Add("Top Selling Items")
+        Panel4.Controls.Add(TopSellingItemChart)
+
+    End Sub
+
+    ' AUTO-REFRESH
+    Private Sub autoRefreshTimer_Tick(sender As Object, e As EventArgs) Handles autoRefreshTimer.Tick
+        Try
+            ApplyFilter()
+        Catch ex As Exception
+            Debug.WriteLine("Auto-refresh failed: " & ex.Message)
+        End Try
+    End Sub
+
+End Class
